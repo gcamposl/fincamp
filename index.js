5cb908bf4525db0a9a350bd27bf2930d7eadec3e
Gabriel Campos Lopes <gcamposlopes@gmail.com>
2021-09-01 11:04:54 -0300

    creating testing in js

---
+++ index.js
@@ -0,0 +1,17 @@
+const client = [
+  {
+    name: 'campos',
+    income: 700.0
+  },
+  {
+    name: 'taj',
+    income: 1200.0
+  },
+  {
+    name: 'joao',
+    income: 2700.0
+  }
+]
+//Inserir elementos na lista usando DOM
+
+//client = document.createElement('li')
