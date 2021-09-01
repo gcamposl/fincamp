2a761af83f9755d7d83c4bb078db2081496d8551
Gabriel Campos Lopes <gcamposlopes@gmail.com>
2021-09-01 11:24:49 -0300

    Insert elements into list using DOM

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ const client = [
     income: 2700.0
   }
 ]
+
+let ul = document.createElement('first-ul')
+
+let btn = document.getElementById('btn')
+
+btn.addEventListener('click', function () {
+  //console.log('clicked')
+
+  client.forEach(function (e) {
+    let li = document.createElement('li')
+    li.innerText = e
+    ul.append(li)
+  })
+})
+
 //Inserir elementos na lista usando DOM
 
 //client = document.createElement('li')
